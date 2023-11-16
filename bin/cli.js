16be049e9e44df259d703a0318eb7e029d906d95
Timothy Jones <timothy.l.jones@gmail.com>
2023-11-16 22:25:37 +1100

    chore: Update formatting (#114)
    * chore: Remove standard lint settings, replace with prettier
    
    * chore: Introduce prettier, and run it over all files
    
    * chore: Remove eslint-config-standard
    
    * chore: Add gitattributes file to fix formatting on windows

--- bin/cli.js
+++ bin/cli.js
@@ -3,12 +3,12 @@
 /* istanbul ignore if */
 if (process.version.match(/v(\d+)\./)[1] < 6) {
   console.error(
-    'commit-and-tag-version: Node v6 or greater is required. `commit-and-tag-version` did not run.'
-  )
+    'commit-and-tag-version: Node v6 or greater is required. `commit-and-tag-version` did not run.',
+  );
 } else {
-  const standardVersion = require('../index')
-  const cmdParser = require('../command')
+  const standardVersion = require('../index');
+  const cmdParser = require('../command');
   standardVersion(cmdParser.argv).catch(() => {
-    process.exit(1)
-  })
+    process.exit(1);
+  });
 }
--- test/config-files.spec.js
+++ test/config-files.spec.js
@@ -1,22 +1,20 @@
-/* global describe it beforeEach afterEach */
+'use strict';
 
-'use strict'
+const shell = require('shelljs');
+const fs = require('fs');
+const { Readable } = require('stream');
+const mockery = require('mockery');
+const stdMocks = require('std-mocks');
 
-const shell = require('shelljs')
-const fs = require('fs')
-const { Readable } = require('stream')
-const mockery = require('mockery')
-const stdMocks = require('std-mocks')
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
 
-const chai = require('chai')
-const expect = chai.expect
-chai.use(require('chai-as-promised'))
-
-function exec () {
-  const cli = require('../command')
-  const opt = cli.parse('commit-and-tag-version')
-  opt.skip = { commit: true, tag: true }
-  return require('../index')(opt)
+function exec() {
+  const cli = require('../command');
+  const opt = cli.parse('commit-and-tag-version');
+  opt.skip = { commit: true, tag: true };
+  return require('../index')(opt);
 }
 
 /**
@@ -28,149 +26,152 @@ function exec () {
  * changelog?: string | Error | Array<string | Error | (opt) => string | null>
  * tags?: string[] | Error
  */
-function mock ({ bump, changelog, tags } = {}) {
-  mockery.enable({ warnOnUnregistered: false, useCleanCache: true })
+function mock({ bump, changelog, tags } = {}) {
+  mockery.enable({ warnOnUnregistered: false, useCleanCache: true });
 
-  mockery.registerMock('conventional-recommended-bump', function (opt, parserOpts, cb) {
-    if (typeof bump === 'function') bump(opt, parserOpts, cb)
-    else if (bump instanceof Error) cb(bump)
-    else cb(null, bump ? { releaseType: bump } : {})
-  })
+  mockery.registerMock(
+    'conventional-recommended-bump',
+    function (opt, parserOpts, cb) {
+      if (typeof bump === 'function') bump(opt, parserOpts, cb);
+      else if (bump instanceof Error) cb(bump);
+      else cb(null, bump ? { releaseType: bump } : {});
+    },
+  );
 
-  if (!Array.isArray(changelog)) changelog = [changelog]
+  if (!Array.isArray(changelog)) changelog = [changelog];
   mockery.registerMock(
     'conventional-changelog',
     (opt) =>
       new Readable({
-        read (_size) {
-          const next = changelog.shift()
+        read(_size) {
+          const next = changelog.shift();
           if (next instanceof Error) {
-            this.destroy(next)
+            this.destroy(next);
           } else if (typeof next === 'function') {
-            this.push(next(opt))
+            this.push(next(opt));
           } else {
-            this.push(next ? Buffer.from(next, 'utf8') : null)
+            this.push(next ? Buffer.from(next, 'utf8') : null);
           }
-        }
-      })
-  )
+        },
+      }),
+  );
 
   mockery.registerMock('git-semver-tags', function (cb) {
-    if (tags instanceof Error) cb(tags)
-    else cb(null, tags | [])
-  })
+    if (tags instanceof Error) cb(tags);
+    else cb(null, tags | []);
+  });
 
-  stdMocks.use()
-  return () => stdMocks.flush()
+  stdMocks.use();
+  return () => stdMocks.flush();
 }
 
 describe('config files', function () {
   beforeEach(function () {
-    shell.rm('-rf', 'tmp')
-    shell.config.silent = true
-    shell.mkdir('tmp')
-    shell.cd('tmp')
+    shell.rm('-rf', 'tmp');
+    shell.config.silent = true;
+    shell.mkdir('tmp');
+    shell.cd('tmp');
     fs.writeFileSync(
       'package.json',
       JSON.stringify({ version: '1.0.0' }),
-      'utf-8'
-    )
-  })
+      'utf-8',
+    );
+  });
 
   afterEach(function () {
-    shell.cd('../')
-    shell.rm('-rf', 'tmp')
+    shell.cd('../');
+    shell.rm('-rf', 'tmp');
 
-    mockery.deregisterAll()
-    mockery.disable()
-    stdMocks.restore()
+    mockery.deregisterAll();
+    mockery.disable();
+    stdMocks.restore();
 
     // push out prints from the Mocha reporter
-    const { stdout } = stdMocks.flush()
+    const { stdout } = stdMocks.flush();
     for (const str of stdout) {
-      if (str.startsWith(' ')) process.stdout.write(str)
+      if (str.startsWith(' ')) process.stdout.write(str);
     }
-  })
+  });
 
-  const configKeys = ['commit-and-tag-version', 'standard-version']
+  const configKeys = ['commit-and-tag-version', 'standard-version'];
 
   configKeys.forEach((configKey) => {
     it(`reads config from package.json key '${configKey}'`, async function () {
       const issueUrlFormat =
-        'https://commit-and-tag-version.company.net/browse/{{id}}'
+        'https://commit-and-tag-version.company.net/browse/{{id}}';
       mock({
         bump: 'minor',
-        changelog: ({ preset }) => preset.issueUrlFormat
-      })
+        changelog: ({ preset }) => preset.issueUrlFormat,
+      });
       const pkg = {
         version: '1.0.0',
         repository: { url: 'git+https://company@scm.org/office/app.git' },
-        [configKey]: { issueUrlFormat }
-      }
-      fs.writeFileSync('package.json', JSON.stringify(pkg), 'utf-8')
+        [configKey]: { issueUrlFormat },
+      };
+      fs.writeFileSync('package.json', JSON.stringify(pkg), 'utf-8');
 
-      await exec()
-      const content = fs.readFileSync('CHANGELOG.md', 'utf-8')
-      content.should.include(issueUrlFormat)
-    })
-  })
+      await exec();
+      const content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+      content.should.include(issueUrlFormat);
+    });
+  });
 
   it('reads config from .versionrc', async function () {
-    const issueUrlFormat = 'http://www.foo.com/{{id}}'
-    const changelog = ({ preset }) => preset.issueUrlFormat
-    mock({ bump: 'minor', changelog })
-    fs.writeFileSync('.versionrc', JSON.stringify({ issueUrlFormat }), 'utf-8')
+    const issueUrlFormat = 'http://www.foo.com/{{id}}';
+    const changelog = ({ preset }) => preset.issueUrlFormat;
+    mock({ bump: 'minor', changelog });
+    fs.writeFileSync('.versionrc', JSON.stringify({ issueUrlFormat }), 'utf-8');
 
-    await exec()
-    const content = fs.readFileSync('CHANGELOG.md', 'utf-8')
-    content.should.include(issueUrlFormat)
-  })
+    await exec();
+    const content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+    content.should.include(issueUrlFormat);
+  });
 
   it('reads config from .versionrc.json', async function () {
-    const issueUrlFormat = 'http://www.foo.com/{{id}}'
-    const changelog = ({ preset }) => preset.issueUrlFormat
-    mock({ bump: 'minor', changelog })
+    const issueUrlFormat = 'http://www.foo.com/{{id}}';
+    const changelog = ({ preset }) => preset.issueUrlFormat;
+    mock({ bump: 'minor', changelog });
     fs.writeFileSync(
       '.versionrc.json',
       JSON.stringify({ issueUrlFormat }),
-      'utf-8'
-    )
+      'utf-8',
+    );
 
-    await exec()
-    const content = fs.readFileSync('CHANGELOG.md', 'utf-8')
-    content.should.include(issueUrlFormat)
-  })
+    await exec();
+    const content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+    content.should.include(issueUrlFormat);
+  });
 
   it('evaluates a config-function from .versionrc.js', async function () {
-    const issueUrlFormat = 'http://www.foo.com/{{id}}'
+    const issueUrlFormat = 'http://www.foo.com/{{id}}';
     const src = `module.exports = function() { return ${JSON.stringify({
-      issueUrlFormat
-    })} }`
-    const changelog = ({ preset }) => preset.issueUrlFormat
-    mock({ bump: 'minor', changelog })
-    fs.writeFileSync('.versionrc.js', src, 'utf-8')
+      issueUrlFormat,
+    })} }`;
+    const changelog = ({ preset }) => preset.issueUrlFormat;
+    mock({ bump: 'minor', changelog });
+    fs.writeFileSync('.versionrc.js', src, 'utf-8');
 
-    await exec()
-    const content = fs.readFileSync('CHANGELOG.md', 'utf-8')
-    content.should.include(issueUrlFormat)
-  })
+    await exec();
+    const content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+    content.should.include(issueUrlFormat);
+  });
 
   it('evaluates a config-object from .versionrc.js', async function () {
-    const issueUrlFormat = 'http://www.foo.com/{{id}}'
-    const src = `module.exports = ${JSON.stringify({ issueUrlFormat })}`
-    const changelog = ({ preset }) => preset.issueUrlFormat
-    mock({ bump: 'minor', changelog })
-    fs.writeFileSync('.versionrc.js', src, 'utf-8')
+    const issueUrlFormat = 'http://www.foo.com/{{id}}';
+    const src = `module.exports = ${JSON.stringify({ issueUrlFormat })}`;
+    const changelog = ({ preset }) => preset.issueUrlFormat;
+    mock({ bump: 'minor', changelog });
+    fs.writeFileSync('.versionrc.js', src, 'utf-8');
 
-    await exec()
-    const content = fs.readFileSync('CHANGELOG.md', 'utf-8')
-    content.should.include(issueUrlFormat)
-  })
+    await exec();
+    const content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+    content.should.include(issueUrlFormat);
+  });
 
   it('throws an error when a non-object is returned from .versionrc.js', async function () {
-    mock({ bump: 'minor' })
-    fs.writeFileSync('.versionrc.js', 'module.exports = 3', 'utf-8')
+    mock({ bump: 'minor' });
+    fs.writeFileSync('.versionrc.js', 'module.exports = 3', 'utf-8');
 
-    expect(exec).to.throw(/Invalid configuration/)
-  })
-})
+    expect(exec).to.throw(/Invalid configuration/);
+  });
+});
